5e49d9e0447b0f4c1f5967ecb5973f2860349a2c
atiltt <artemgaspary@yandex.ru>
2025-05-19 08:14:21 +0600

    maybe final save;)

--- task_4/app/demo.cpp
+++ task_4/app/demo.cpp
@@ -2,7 +2,6 @@
 #include "Queue.h"
 
 int main() {
-    // Создаём очередь с тремя элементами
     PriorityDeque q = {
         {10, 3},
         {5, 1},
@@ -11,17 +10,13 @@ int main() {
 
     std::cout << "Начальная очередь: " << q.toString() << "\n";
 
-    // Получаем элемент с минимальным и максимальным приоритетом
     auto min = q.get_minimal_priority();
     auto max = q.get_maximum_priority();
 
     std::cout << "Минимальный приоритет: " << min.first << " (pr=" << min.second << ")\n";
     std::cout << "Максимальный приоритет: " << max.first << " (pr=" << max.second << ")\n";
 
-    // Удаляем элемент с минимальным приоритетом
     q.pop_front();
-
-    // Добавляем элементы с новыми приоритетами
     q.push_back(100, 500);
     q.push_front(200, 900);
 
